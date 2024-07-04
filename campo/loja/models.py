50b7df280a549e133369c25cfab67a2cf1a67437
Isaias <60967597+Isaias79@users.noreply.github.com>
2024-07-04 18:30:36 +0000

    Novas

--- campo/loja/models.py
+++ campo/loja/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    preco = models.IntegerField()
+    
+    def __str__(self):
+        return self.nome
+    #quantidade = 
+
+# COLOCAR FLOAT DA PROBLEMA NA PROGAMAÇÃO
 # Create your models here.
